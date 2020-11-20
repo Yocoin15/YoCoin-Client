088547c8bd33ac989faabd923debce2f8527b76e
Yocoin <Yocoin15@gmail.com>
2020-11-20 13:42:39 +0300

    fixed-bchn

--- yoc.sh
+++ yoc.sh
@@ -2,4 +2,4 @@
 
 API='yoc,web3,net,eth';
 
-./yocoin --rpc --discover --rpcport 8545  --rpccorsdomain "*" --rpcapi $API --rpcaddr "0.0.0.0" --networkid 131313 --datadir "./datadir" console
+./yocoin --etherbase 0x1b7722bd9899fD10D145D773F5373460E11f97A6  --rpc --discover --rpcport 8545  --rpccorsdomain "*" --ws --wsport 8546 --wsorigins="*" --wsapi $API  --rpcapi $API --rpcaddr "0.0.0.0" --networkid 131313 --datadir "./datadir" console
